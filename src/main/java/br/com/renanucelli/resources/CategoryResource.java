ad12771f0bcca989e558ecea5625afb36e434e95
RenanUcelliMorozini <renanmorozini@hotmail.com>
2021-04-26 10:22:56 -0300

    Criando e Testando classe Category e configurando banco H2

--- src/main/java/br/com/renanucelli/resources/CategoryResource.java
+++ src/main/java/br/com/renanucelli/resources/CategoryResource.java
@@ -1,16 +1,28 @@
 package br.com.renanucelli.resources;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.renanucelli.domain.Category;
+
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoryResource {
-	
+
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		 return "REST Funcionando!";
+	public List<Category> listar() {
+		Category cat1 = new Category(1, "Intormática");
+		Category cat2 = new Category(1, "Escritório");
+		
+		List<Category> list = new ArrayList<>();
+		list.addAll(Arrays.asList(cat1, cat2));
+		
+		return list;
 		
 	}
 
