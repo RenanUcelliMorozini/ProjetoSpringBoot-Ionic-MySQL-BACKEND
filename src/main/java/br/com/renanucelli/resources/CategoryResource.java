3063494a40f622d178577a63f7fff023b0ed6f74
RenanUcelliMorozini <renanmorozini@hotmail.com>
2021-04-26 09:57:05 -0300

    Testando o REST

---
+++ src/main/java/br/com/renanucelli/resources/CategoryResource.java
@@ -0,0 +1,17 @@
+package br.com.renanucelli.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/categories")
+public class CategoryResource {
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public String listar() {
+		 return "REST Funcionando!";
+		
+	}
+
+}
